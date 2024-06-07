45b46d5386f3b65e77bb07902f068fb292322997
Bruninhamgoliveira <brunamgoliveira@hotmail.com>
2024-06-07 19:04:11 -0300

    Instalção taiwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/ag-server-grid/src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}", // Inclua também os arquivos do aplicativo principal, se necessário
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
