57c5f3a86fea637026bfe2e44a2eca0dd499e0de
Bruninhamgoliveira <brunamgoliveira@hotmail.com>
2024-07-10 14:53:37 -0300

    Ajuste na paginação

--- projects/ag-server-grid/src/lib/datatable-server/components/pagination-server/service/pager.service.ts
+++ projects/ag-server-grid/src/lib/datatable-server/components/pagination-server/service/pager.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 @Injectable()
 export class PagerService {
-
-  getPager(totalItems: number, currentPage: number = 1, pageSize: number = 10): Object {
+  getPager(totalItems: number, currentPage: number = 0, pageSize: number = 10): Object {
     // calculate total pages
     let totalPages = Math.ceil(totalItems / pageSize);
 
     // ensure current page isn't out of range
-    if (currentPage < 1) {
-      currentPage = 1;
+    if (currentPage < 0) {
+      currentPage = 0;
     } else if (currentPage > totalPages) {
       currentPage = totalPages;
     }
@@ -16,12 +15,12 @@ export class PagerService {
     let startPage: number, endPage: number;
     if (totalPages <= 10) {
       // less than 10 total pages so show all
-      startPage = 1;
+      startPage = 0;
       endPage = totalPages;
     } else {
       // more than 10 total pages so calculate start and end pages
       if (currentPage <= 6) {
-        startPage = 1;
+        startPage = 0;
         endPage = 10;
       } else if (currentPage + 4 >= totalPages) {
         startPage = totalPages - 9;
